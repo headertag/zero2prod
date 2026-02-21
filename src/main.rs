0b12be68800c8041d3a8c69af79208a7a597de52
Jourdain-Alexander Casale <jourdain@indexexchange.com>
2026-02-21 16:13:14 -0500

    fix rustfmt warns part deux

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     println!("Hello, world!");
     println!("-3 to the power of 2 is:");
-    println!("{}", power( -3, 2));
+    println!("{}", power(-3, 2));
 }
 
-fn power( base: i64, n:i64 ) -> i64 {
+fn power(base: i64, n:i64 ) -> i64 {
     assert!(base != 0 && n != 0);
     let mut i = 1;
     let mut calc = base;
@@ -17,6 +17,6 @@ fn power( base: i64, n:i64 ) -> i64 {
 
 #[test]
 fn test_power() {
-    assert_eq!( power( -3, 2 ), 9);
-    assert_eq!( power( -3, 3 ), -27);
+    assert_eq!(power( -3, 2 ), 9);
+    assert_eq!(power( -3, 3 ), -27);
 }
