06ce435698cd72c582edc22e751ee4bf75d60a07
Jourdain-Alexander Casale <jourdain@indexexchange.com>
2026-02-21 16:03:56 -0500

    added a simple function and test case for example run through the CI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
     println!("Hello, world!");
-    println!("Way cool");
+    println!("-3 to the power of 2 is:");
+    println!("{}", power( -3, 2 ) );
+}
+
+fn power( base: i64, n:i64 ) -> i64
+{
+    assert!( base != 0 && n != 0 );
+    let mut i = 1;
+    let mut calc = base;
+    while i < n {
+        calc = calc * base;
+        i = i + 1;
+    }
+    calc
+}
+
+#[test]
+fn test_power() {
+    assert_eq!( power( -3, 2 ), 9);
+    assert_eq!( power( -3, 3 ), -27);
 }
